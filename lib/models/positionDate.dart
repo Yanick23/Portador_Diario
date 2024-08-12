20ab3c6854b9c25c0bd70f05f9d44a9b347b1c13
Yanick23 <yanick.ribeiro23@gmail.com>
2024-08-12 12:33:08 +0200

    feat: Ultimo lacamento e discrofrafia em ordem

---
+++ lib/models/positionDate.dart
@@ -0,0 +1,7 @@
+class PositionData {
+  const PositionData(this.position, this.bufferedPOsition, this.duration);
+
+  final Duration position;
+  final Duration bufferedPOsition;
+  final Duration duration;
+}
